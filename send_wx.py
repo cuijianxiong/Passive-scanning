02a27251147c34cdc44e77e12efd14c04403a5f3
cuijianxiong <207426719@qq.com>
2018-11-21 15:22:32 +0800

    delete  information

--- send_wx.py
+++ send_wx.py
@@ -4,7 +4,7 @@ import json
 
 '''微信通知接口   需要修改token_url corpid corpsecret的值以及agentid应用id'''
 
-token_url = "https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=wwa4c406ce6d8457c0&corpsecret=ZrKF2JmDGj3boIPa1c_3qRZDD7qHgzVRw3zn8PNVIuw"
+token_url = "https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=xxx&corpsecret=xxx"
 
 def send_data(data):
     tok = requests.get(url=token_url)
